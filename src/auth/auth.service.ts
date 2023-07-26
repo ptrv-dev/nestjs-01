bcb0d658065d33e40f4f5bd5ae63fa246918d399
ptrv-dev <petrov99119@gmail.com>
2023-07-26 21:15:10 +0300

    auth: auth guard role check, role decorator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -36,6 +36,7 @@ export class AuthService {
     const payload = {
       id: user.id,
       email: user.email,
+      role: user.role,
     };
     const token = await this.jwtService.signAsync(payload);
     return token;
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,27 +1,45 @@
 import {
   CanActivate,
   ExecutionContext,
+  ForbiddenException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.getAllAndOverride(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     const request: Request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeaders(request);
     if (!token) throw new UnauthorizedException();
 
+    let payload: any = {};
+
     try {
-      const payload = await this.jwtService.verifyAsync(token);
+      payload = await this.jwtService.verifyAsync(token);
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
     }
+
+    if (roles && roles instanceof Array && roles.length > 0) {
+      const hasRole = roles.some(
+        (role: string) => role.toUpperCase() === payload.role?.toUpperCase(),
+      );
+      if (!hasRole) throw new ForbiddenException();
+    }
+
     return true;
   }
 
