5d8e7293a5ba1ee9ed86ff117b9556174d483db1
ptrv-dev <petrov99119@gmail.com>
2023-07-26 21:15:37 +0300

    user: controller get all set roles to ADMIN

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @UseGuards(AuthGuard)
+  @Roles('ADMIN')
   @Get()
   async getAll() {
     const users = await this.userService.getAll();
